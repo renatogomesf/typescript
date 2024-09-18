23b056c1cee156376dae99efda8d00d73776ec4c
renatogomesf <renato_gfe@hotmail.com>
2024-09-18 18:06:56 -0300

    feita aula 07

---
+++ modulo01/helloworld/app.js
@@ -0,0 +1,6 @@
+var mensagem = "hello world";
+console.log(mensagem);
+function soma(num1, num2) {
+    return num1 + num2;
+}
+console.log(soma(2, 4));
---
+++ modulo01/helloworld/app.ts
@@ -0,0 +1,9 @@
+let mensagem: string = "hello world";
+
+console.log(mensagem);
+
+function soma(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(soma(2, 4));
