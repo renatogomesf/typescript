e88b782c17dce09adaa9ffb9644e5a6c16c2717f
renatogomesf <renato_gfe@hotmail.com>
2024-09-24 12:03:42 -0300

    feita aula 32.1

--- modulo02/aula32 e 32.1/objectType.ts
+++ modulo02/aula32 e 32.1/objectType.ts
@@ -61,21 +61,103 @@ interface Pessoa04 {
 }
 
 function onboarding04(pessoa: Pessoa04) {
-    return `Seja bem-vindo ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}. Você trabalhara com a linguagem ${pessoa.linguagem}`;
-  }
-  
-  console.log(
-    onboarding04({
-      nome: 'Renato',
-      funcao: 'Eng.Software',
-      linguagem: 'JavaScript/TypeScript'
-    }),
-  );
+  return `Seja bem-vindo ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}. Você trabalhara com a linguagem ${pessoa.linguagem}`;
+}
+
+console.log(
+  onboarding04({
+    nome: 'Renato',
+    funcao: 'Eng.Software',
+    linguagem: 'JavaScript/TypeScript',
+  }),
+);
 
 // Exemplo 06 - Propriedade 'readonly' (se deseja proibir que os devs não modifiquem um determinado objeto use o 'readonly')
+interface Pessoa05 {
+  nome: string;
+  funcao: string;
+  linguagem: string;
+  readonly email: string; // não pode ser gravada durante a verificação de tipo.
+}
+
+function onboarding05(pessoa: Pessoa05) {
+  return `Seja bem-vindo ${pessoa.nome}! Sua função aqui na empresa será ${pessoa.funcao}. Você trabalhara com a linguagem ${pessoa.linguagem}. Seu e-mail será ${pessoa.email}`;
+}
+
+console.log(
+  onboarding05({
+    nome: 'Renato',
+    funcao: 'Eng.Software',
+    linguagem: 'JavaScript/TypeScript',
+    email: 'renatogomes@microsof.com',
+  }),
+);
 
 // Exemplo 07 - Tipos de exensões (heranças)
+interface Mae {
+  nome: string;
+}
 
-// Exemplo 08 - Tipos de interseções
+interface Pai {
+  sobrenome: string;
+}
+
+interface Filha extends Mae, Pai {
+  idade: number;
+}
+
+const filha: Filha = {
+  nome: 'glaucia',
+  sobrenome: 'lemos',
+  idade: 34,
+};
+
+console.log(filha);
+
+// Exemplo 08 - Tipos de Interseções
+interface Cachorro {
+  tipo: string;
+}
+
+interface Gato {
+  tipo: string;
+}
+
+type Animal = Cachorro & Gato; //Interseção
 
 // Exemplo 09 - Generic Object
+type Usuario = {
+  nome: string;
+  email: string;
+};
+
+type Admim = {
+  nome: string;
+  email: string;
+  admim: true;
+};
+
+const usuario: Usuario = {
+  nome: 'glaucia lemos',
+  email: 'algumacoisa@gmail.com',
+};
+
+const admim: Admim = {
+  nome: 'glaucia lemos',
+  email: 'algumacoisa@gmail.com',
+  admim: true,
+};
+
+//"<T>": pode ser outra letra... mas usando o "T", indica que é uma função do tipo genérico. aceita informações de usuário e de admim
+function acessarSistema<T>(usuario: T): T {
+  return usuario;
+}
+
+console.log(acessarSistema<Usuario>(usuario));
+console.log(acessarSistema<Admim>(admim));
+
+// function acessarSistema(usuario: Usuario): Usuario {
+//   return usuario;
+// }
+
+// console.log(acessarSistema(usuario));
