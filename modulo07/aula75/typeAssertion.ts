4f7462fdec088c35ff34c3636141b94b27481ed7
renatogomesf <renato_gfe@hotmail.com>
2024-10-28 17:53:29 -0300

    feita aula 75

--- modulo07/aula75/typeAssertion.ts
+++ modulo07/aula75/typeAssertion.ts
@@ -1,5 +1,5 @@
 
-// informa ao compilador sobre o tipo de uma variável
+// informa ao compilador sobre o tipo de uma variável. intrui como deve tratar.
 
 // semelhante ao Type Casting, mas não reconstroe o código.
 
@@ -7,3 +7,50 @@
 
 // EVITAR O USO DE TYPE ASSERTION
 
+export {}
+
+// Exemplo 01 - Type Assertion ('as' / '<>')
+
+function exibirPrecoFinal(preco:number, desconto: number, formato: boolean): number | string {
+    const precoComDesconto = preco * (1 - desconto)
+
+    return formato ? `R$ ${precoComDesconto}` : precoComDesconto
+}
+
+// const descontoFinal = exibirPrecoFinal(100, 0.05, true) as string
+
+const descontoFinal = <number>exibirPrecoFinal(100, 0.05, false)
+
+console.log(descontoFinal);
+
+// Exemplo 02 - Type Assertion ('as' / '<>')
+
+// type Humano = {
+//     nome: string
+//     idade: number
+//     idioma: string
+// }
+
+// const humano = {
+//     idade: 36,
+//     idioma: 'português'
+// }
+
+// const humano_02 = humano as Humano
+
+// console.log(humano_02.nome.toUpperCase());
+
+type Humano = {
+    nome: string
+    idade: number
+    idioma: string
+}
+
+const humano = {
+    idade: 36,
+    idioma: 'português'
+}
+
+const humano_02: Humano = humano // type anotation
+
+console.log(humano_02.nome.toUpperCase());
